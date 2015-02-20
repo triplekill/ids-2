47c4f118c0b293647acce0135c7bdf33952029a8
mvalle <mvalle@localhost.localdomain>
2015-02-20 07:49:08 -0500

    Initial project

---
+++ ids/src/ids/Ids.java
@@ -0,0 +1,17 @@
+package ids;
+
+
+/**
+ *
+ * @author mvalle
+ */
+public class Ids {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
